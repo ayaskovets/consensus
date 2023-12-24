2a074f7609b6e2069bb3ba03c250fec9afe537b3
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2023-12-24 23:21:05 +0300

    add rpc registering method for node

--- pkg/node/rpc.go
+++ pkg/node/rpc.go
@@ -1,5 +1,15 @@
 package node
 
+import "fmt"
+
+func (node *Node) Register(rcvr any) error {
+	return node.server.Register(rcvr)
+}
+
 func (node *Node) Call(addr string, serviceMethod string, args any, reply any) error {
-	return node.peers[addr].Call(serviceMethod, args, reply)
+	peer := node.peers[addr]
+	if peer == nil {
+		return fmt.Errorf("rpc call (%s) on unitialized connection %s", serviceMethod, addr)
+	}
+	return peer.Call(serviceMethod, args, reply)
 }
