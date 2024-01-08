06b385b00ae66c1b0da348bbd737b301caae669a
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2024-01-08 17:21:07 +0300

    move interface & rpc args to a separate file

--- pkg/consensus/raft/raft.go
+++ pkg/consensus/raft/raft.go
@@ -16,56 +16,6 @@ const (
 	Nobody string = ""
 )
 
-// RequestVote RPC args
-type RequestVoteArgs struct {
-	Term        int
-	CandidateId string
-}
-
-// RequestVote RPC reply
-type RequestVoteReply struct {
-	Term        int
-	VoteGranted bool
-}
-
-// AppendEntries RPC args
-type AppendEntriesArgs struct {
-	Term     int
-	LeaderId string
-}
-
-// AppendEntries RPC reply
-type AppendEntriesReply struct {
-	Term    int
-	Success bool
-}
-
-// Remote Raft instance interface
-//
-// Implementation is expected to be thread-safe
-type RaftPeer interface {
-	Id() string
-	RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error
-	AppendEntries(args AppendEntriesArgs, reply *AppendEntriesReply) error
-}
-
-// Customizable network topology info provider for local Raft instance
-//
-// Implementation is expected to be thread-safe
-type RaftNode interface {
-	Id() string
-	Peers() []RaftPeer
-}
-
-// Customizable static-ish settings and configuration for local Raft instance
-//
-// Implementation is expected to be thread-safe
-type RaftSettings interface {
-	HeartbeatTimeout() time.Duration
-	ElectionTimeout() time.Duration
-	Majority(peers int) int
-}
-
 // Local Raft instance state
 type Raft struct {
 	// Communication with peers and configuration
