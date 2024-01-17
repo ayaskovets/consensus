14c965ba19027bbf2733d83e986eba4b9dc96c9a
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2024-01-17 21:08:45 +0300

    add raft external interface implementation

---
+++ pkg/consensus/raft/test/implementation.go
@@ -0,0 +1,102 @@
+package test
+
+import (
+	"math/rand"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/ayaskovets/consensus/pkg/consensus/raft"
+	"github.com/ayaskovets/consensus/pkg/node"
+)
+
+// Remote Raft node implementation. Can be used as RPC proxy
+//
+// Uses port number as id. Expects node to be already connected to the addr and running
+type RaftPeer struct {
+	addr net.Addr
+	node *node.Node
+}
+
+func (peer *RaftPeer) Id() string {
+	port := strings.Split(peer.addr.String(), ":")[1]
+	return port
+}
+
+func (peer *RaftPeer) RequestVote(args raft.RequestVoteArgs, reply *raft.RequestVoteReply) error {
+	return peer.node.Call(peer.addr, "Raft.RequestVote", args, reply)
+}
+
+func (peer *RaftPeer) AppendEntries(args raft.AppendEntriesArgs, reply *raft.AppendEntriesReply) error {
+	return peer.node.Call(peer.addr, "Raft.AppendEntries", args, reply)
+}
+
+// Local Raft node implementation
+//
+// Expects node to be already up and running. Does not handle startup or
+// shutdown
+type RaftNode struct {
+	node *node.Node
+	raft *raft.Raft
+}
+
+func (node *RaftNode) Id() string {
+	port := strings.Split(node.node.Addr().String(), ":")[1]
+	return port
+}
+
+func (node *RaftNode) Peers() []raft.RaftPeer {
+	peers := make([]raft.RaftPeer, 0)
+	for _, addr := range node.node.Peers() {
+		peers = append(peers, &RaftPeer{addr: addr, node: node.node})
+	}
+	return peers
+}
+
+// Mock local Raft node to test basic function without starting a real network
+// server
+type MockRaftNode struct {
+}
+
+func (node *MockRaftNode) Id() string {
+	return "mockId"
+}
+
+func (node *MockRaftNode) Peers() []raft.RaftPeer {
+	return []raft.RaftPeer{}
+}
+
+// Implementation of raft.RaftSettings
+//
+// Default timeouts are the ones suggested in the original paper
+type RaftSettings struct {
+	MinElectionTimeout  int
+	MaxElectionTimeout  int
+	MinHeartbeatTimeout int
+	MaxHeartbeatTimeout int
+}
+
+func NewRaftSettings() RaftSettings {
+	return RaftSettings{
+		MinElectionTimeout:  150,
+		MaxElectionTimeout:  300,
+		MinHeartbeatTimeout: 50,
+		MaxHeartbeatTimeout: 100,
+	}
+}
+
+func (settings RaftSettings) ElectionTimeout() time.Duration {
+	min := settings.MinElectionTimeout
+	max := settings.MaxElectionTimeout
+	return time.Millisecond * time.Duration(rand.Intn(max-min)+min)
+}
+
+func (settings RaftSettings) HeartbeatTimeout() time.Duration {
+	min := settings.MinHeartbeatTimeout
+	max := settings.MaxHeartbeatTimeout
+	return time.Millisecond * time.Duration(rand.Intn(max-min)+min)
+}
+
+func (settings RaftSettings) Majority(peers int) int {
+	return peers/2 + 1
+}
