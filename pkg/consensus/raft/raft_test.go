c99ea1ca36db2a880beb8fa1fd8f0bbb2bd28881
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2024-01-17 23:27:44 +0300

    rework tests

--- pkg/consensus/raft/raft_test.go
+++ pkg/consensus/raft/raft_test.go
@@ -29,14 +29,14 @@ func addrs(n int) []net.Addr {
 }
 
 func TestRestart(t *testing.T) {
-	cns := raft.NewRaft(&test.MockRaftNode{}, &test.RaftSettings{})
+	cns := raft.NewRaft(&test.MockRaftNode{}, test.NewRaftSettings())
 
 	assert.Nil(t, cns.Up())
 	assert.Nil(t, cns.Down())
 }
 
 func TestIdempotency(t *testing.T) {
-	cns := raft.NewRaft(&test.MockRaftNode{}, &test.RaftSettings{})
+	cns := raft.NewRaft(&test.MockRaftNode{}, test.NewRaftSettings())
 
 	assert.Nil(t, cns.Up())
 	assert.Nil(t, cns.Up())
@@ -45,41 +45,58 @@ func TestIdempotency(t *testing.T) {
 }
 
 func TestElection(t *testing.T) {
-	cluster := test.WithCluster(t, addrs(5))
+	cluster := test.WithCluster(t, addrs(3))
 
-	// Assert that a single leader is elected
-	state1 := cluster.WaitElection().GetState()
+	// Assert that a single leader is elected and there is no servers in
+	// the Candidate state
+	state1 := cluster.
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state1.Leader)
+	assert.Len(t, state1.Followers, 2)
+	assert.Empty(t, state1.Candidates)
 }
 
 func TestFollowerDisconnect(t *testing.T) {
 	cluster := test.WithCluster(t, addrs(3))
 
-	// Assert that a single leader is elected
-	state1 := cluster.WaitElection().GetState()
+	// Assert that the first election is successful
+	state1 := cluster.
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state1.Leader)
-	assert.NotEmpty(t, state1.Followers)
 
 	// Assert that no election occured after the follower disconnected
-	state2 := cluster.Disconnect(state1.Followers[0]).WaitElection().GetState()
+	state2 := cluster.
+		Disconnect(state1.Followers[0]).
+		WaitElection().
+		GetState()
 	assert.Equal(t, state2.Leader, state1.Leader)
 	assert.Equal(t, state2.Term, state1.Term)
 
-	// Assert that after the follower reconnected a new leader is elected
-	state3 := cluster.Reconnect(state1.Followers[0]).WaitElection().GetState()
+	// Assert that after the follower reconnected a new leader was elected
+	state3 := cluster.
+		Reconnect(state1.Followers[0]).
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state3.Leader)
 	assert.Greater(t, state3.Term, state1.Term)
 }
 
 func TestLeaderDisconnect(t *testing.T) {
-	cluster := test.WithCluster(t, addrs(5))
+	cluster := test.WithCluster(t, addrs(3))
 
-	// Assert that a single leader is elected
-	state1 := cluster.WaitElection().GetState()
+	// Assert that the first election is successful
+	state1 := cluster.
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state1.Leader)
 
-	// Assert that after the leader is down a new leader is elected
-	state2 := cluster.Disconnect(state1.Leader).WaitElection().GetState()
+	// Assert that after the leader disconnected a new leader was elected
+	state2 := cluster.
+		Disconnect(state1.Leader).
+		WaitElection().
+		GetState()
 	assert.NotEqual(t, state2.Leader, state1.Leader)
 	assert.Greater(t, state2.Term, state1.Term)
 }
@@ -87,43 +104,66 @@ func TestLeaderDisconnect(t *testing.T) {
 func TestNoQuorum(t *testing.T) {
 	cluster := test.WithCluster(t, addrs(3))
 
-	// Assert that a single leader is elected
-	state1 := cluster.WaitElection().GetState()
+	// Assert that the first election is successful
+	state1 := cluster.
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state1.Leader)
 
 	// Assert that after the leader is down a new leader is elected
-	state2 := cluster.Disconnect(state1.Leader).WaitElection().GetState()
+	state2 := cluster.
+		Disconnect(state1.Leader).
+		WaitElection().
+		GetState()
 	assert.NotEqual(t, state2.Leader, state1.Leader)
 	assert.Greater(t, state2.Term, state1.Term)
 
 	// Assert that for 1/3 alive nodes there is no elected leader
-	state3 := cluster.Disconnect(state2.Leader).WaitElection().GetState()
+	state3 := cluster.
+		Disconnect(state2.Leader).
+		WaitElection().
+		GetState()
 	assert.Nil(t, state3.Leader)
 }
 
 func TestNoQuorumReconnect(t *testing.T) {
 	cluster := test.WithCluster(t, addrs(3))
 
-	// Assert that a single leader is elected
-	state1 := cluster.WaitElection().GetState()
+	// Assert that the first election is successful
+	state1 := cluster.
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state1.Leader)
 
 	// Assert that after the leader is down a new leader is elected
-	state2 := cluster.Disconnect(state1.Leader).WaitElection().GetState()
+	state2 := cluster.
+		Disconnect(state1.Leader).
+		WaitElection().
+		GetState()
 	assert.NotEqual(t, state2.Leader, state1.Leader)
 	assert.Greater(t, state2.Term, state1.Term)
 
 	// Assert that for 1/3 alive nodes there is no elected leader
-	state3 := cluster.Disconnect(state2.Leader).WaitElection().GetState()
+	state3 := cluster.
+		Disconnect(state2.Leader).
+		WaitElection().
+		GetState()
 	assert.Nil(t, state3.Leader)
+	assert.GreaterOrEqual(t, state3.Term, state2.Term)
 
-	// Assert that for 2/3 alive nodes after the reconnect a new leader is elected
-	state4 := cluster.Reconnect(state2.Leader).WaitElection().GetState()
+	// Assert that for 2/3 alive nodes after a new leader is elected
+	state4 := cluster.
+		Reconnect(state2.Leader).
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state4.Leader)
 	assert.Greater(t, state4.Term, state3.Term)
 
 	// Assert that after all nodes come back there is still a single leader
-	state5 := cluster.Reconnect(state1.Leader).WaitElection().GetState()
+	state5 := cluster.
+		Reconnect(state1.Leader).
+		WaitElection().
+		GetState()
 	assert.NotNil(t, state5.Leader)
-	assert.GreaterOrEqual(t, state4.Term, state3.Term)
+	assert.GreaterOrEqual(t, state5.Term, state4.Term)
 }
